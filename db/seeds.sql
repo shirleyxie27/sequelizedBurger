37d47ac42d7f1e3b6172d85aae5fd0dd8211f12e
Shirley <shirleyxie27@gmail.com>
2017-07-20 22:25:58 -0700

    adding files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Spicy Chicken Deluxe');
+INSERT INTO burgers (name) VALUES ('Turkey Club Sammy');
+INSERT INTO burgers (name, devoured) VALUES ('Chicken Bacon Avocado', true);
+INSERT INTO burgers (name, devoured) VALUES ('Bison Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('NY Strip Burger', true);
+INSERT INTO burgers (name) VALUES ('Venison Burger');
